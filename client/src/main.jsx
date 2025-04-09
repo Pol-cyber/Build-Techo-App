6277d72f4d9e5b971b2c74f7c0c9a3435fcccaf1
Pavlo Yarema <yarema.ketta2002@gmail.com>
2025-04-09 13:52:20 +0300

    Added css variables

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router.jsx";
+import "./variables.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
