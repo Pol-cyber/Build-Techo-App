c119700b0493f063e6739dd5d3f6ec970ac91a90
rk800-afk git config --global user.name <gtv200013@gmail.com>
2025-04-09 13:57:41 +0300

    repaired folder structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router/router.jsx";
-import "./variables.css";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./styles/index.css"
+import App from "./App.jsx"
+import { RouterProvider } from "react-router-dom"
+import { router } from "./router/router.jsx"
+import "./styles/variables.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -12,4 +12,4 @@ createRoot(document.getElementById("root")).render(
       <App />
     </RouterProvider>
   </StrictMode>
-);
+)
